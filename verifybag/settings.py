f2abe2db9283314f3ba500a65c20104ec60f0d9f
Tobiverse-18 <allibaloguntomiwa@gmail.com>
2026-07-21 23:07:53 +0100

    improve home and navbar

--- verifybag/settings.py
+++ verifybag/settings.py
@@ -12,17 +12,24 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-local-development-secret-key"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
