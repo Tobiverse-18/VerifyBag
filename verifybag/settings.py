b4a0074e83c5500227b2ade12ddd866e017619f0
Tobiverse-18 <allibaloguntomiwa@gmail.com>
2026-07-22 01:26:15 +0100

    fix gmail

--- verifybag/settings.py
+++ verifybag/settings.py
@@ -150,7 +150,7 @@ EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "verifybagteam@gmail.com")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
